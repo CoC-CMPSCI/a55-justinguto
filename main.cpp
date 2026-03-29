6ec36d1427c83ea1a1b0bf6b60955f8115c9d27f
justinguto <jutogustowski@gmail.com>
2026-03-29 12:29:27 -0700

    Complete a55 student score totals and averages

--- main.cpp
+++ main.cpp
@@ -1,31 +1,37 @@
-
 #include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
 int main()
 {
-	int N; // the number of students;
-	int M; // the number of scores;
+    int N; // number of students
+    int M; // number of scores
 
-	cout << "Enter the number of students: " ;
-	cin >> N;
-	cout << "Enter the number of scores for each student: " ;
-	cin >> M;
+    cout << "Enter the number of students: ";
+    cin >> N;
+    cout << "Enter the number of scores for each student: ";
+    cin >> M;
 
-	// TODO: declare variables for student name, score, sum, average
+    string name;
+    int score, sum;
+    double average;
 
-	for(int i=0; i<N; i++)
-	{
-		// TODO: read student name and initialize sum
+    for (int i = 0; i < N; i++)
+    {
+        cin >> name;
+        sum = 0;
 
-		for (int j=0; j<M; j++)
-		{
-			// TODO: read each score and accumulate sum
-		}
+        for (int j = 0; j < M; j++)
+        {
+            cin >> score;
+            sum += score;
+        }
 
-		// TODO: calculate average and print name, sum, average
-	}
+        average = static_cast<double>(sum) / M;
 
-	return 0;
+        cout << name << " " << sum << " " << fixed << setprecision(0) << average << endl;
+    }
+
+    return 0;
 }
